80dff0f0c57da11ed7e8f78b8ee7a59ac7344bb0
karynina3 <karynina3@gmail.com>
2022-05-11 20:49:18 -0500

    primer comentario

---
+++ tethysapp/sonic_test/app.py
@@ -0,0 +1,34 @@
+from tethys_sdk.base import TethysAppBase, url_map_maker
+
+
+class SonicTest(TethysAppBase):
+    """
+    Tethys app class for Sonic Test.
+    """
+
+    name = 'Sonic Test'
+    index = 'sonic_test:home'
+    icon = 'sonic_test/images/icon.gif'
+    package = 'sonic_test'
+    root_url = 'sonic-test'
+    color = '#2c3e50'
+    description = ''
+    tags = ''
+    enable_feedback = False
+    feedback_emails = []
+
+    def url_maps(self):
+        """
+        Add controllers
+        """
+        UrlMap = url_map_maker(self.root_url)
+
+        url_maps = (
+            UrlMap(
+                name='home',
+                url='sonic-test',
+                controller='sonic_test.controllers.home'
+            ),
+        )
+
+        return url_maps
